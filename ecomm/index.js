a026a03db540e798fd79a1b1c8304536df5424bf
Amish Pratap Singh <amishpratapsingh13@gmail.com>
2023-06-26 16:17:34 +0530

    middle ware using body parser for handling the form data

--- ecomm/index.js
+++ ecomm/index.js
@@ -1,5 +1,7 @@
 const express = require('express');
+const bodyParser=require('body-parser');
 const app = express();
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.get('/', (req, res) => {
     res.send(`
@@ -15,24 +17,8 @@ app.get('/', (req, res) => {
 });
 
 //middleware
-const bodyParser = (req, res, next) => {
-    if (req.method === 'POST') {
-        req.on('data', data => {
-            const parsed = data.toString('utf8').split('&');
-            const formData = {};
-            for (let pair of parsed) {
-                const [key, value] = pair.split('=');
-                formData[key] = value;
-            }
-            req.body=formData;
-            next();
-        })
-    }else{
-        next();
-    }
-}
-app.post('/', bodyParser,(req, res) => {
-
+app.post('/', (req, res) => {
+    console.log(req.body);
     res.send('Account created');
 })
 app.listen(3000, () => {
