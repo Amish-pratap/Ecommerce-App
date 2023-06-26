1a67c572e8707ee68e963f26dc45b4a9d6be19b8
Amish Pratap Singh <amishpratapsingh13@gmail.com>
2023-06-26 16:09:47 +0530

    express middleware

--- ecomm/index.js
+++ ecomm/index.js
@@ -1,1 +1,40 @@
-console.log('hi there');
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send(`
+        <div>
+            <form method="POST">
+                <input name="email" placeholder="email" />
+                <input name="password" placeholder="password"/>
+                <input name="passwordConfirmation" placeholder="password Confirntion"/>
+                <button>Sign Up</button>
+            </form>
+        </div>
+    `);
+});
+
+//middleware
+const bodyParser = (req, res, next) => {
+    if (req.method === 'POST') {
+        req.on('data', data => {
+            const parsed = data.toString('utf8').split('&');
+            const formData = {};
+            for (let pair of parsed) {
+                const [key, value] = pair.split('=');
+                formData[key] = value;
+            }
+            req.body=formData;
+            next();
+        })
+    }else{
+        next();
+    }
+}
+app.post('/', bodyParser,(req, res) => {
+
+    res.send('Account created');
+})
+app.listen(3000, () => {
+    console.log('Listening');
+}) 
