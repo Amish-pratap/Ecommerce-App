b974f8f44ad2b0fa92ab282e1719973d18209d57
Amish Pratap Singh <amishpratapsingh13@gmail.com>
2023-07-01 16:03:25 +0530

    form to add new products

--- ecomm/routes/admin/products.js
+++ ecomm/routes/admin/products.js
@@ -1,9 +1,12 @@
 const express = require('express');
-
+const productosNewTemplate = require('../../views/admin/products/new');
+const productsRepo= require('../../repositories/products');
 const router = express.Router();
 
 router.get('/admin/products', (req, res) => {});
 
-router.get('/admin/products/new', (req, res) => {});
+router.get('/admin/products/new', (req, res) => {
+    res.send(productosNewTemplate({}));
+});
 
 module.exports = router;
